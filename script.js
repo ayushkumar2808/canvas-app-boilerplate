6fe2ed195490e5e6026cbde757d6b28437995526
Ayush <ayushkumar28082004@gmail.com>
2022-09-13 17:55:54 +0530

    adding html

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 /* Iteration 4: Make the Everything Work */
+constfrozenbackground = document.getElementById("frozenbackground");
+frozenbackground.oninput = () => {
+  background.style.fill = frozenbackground.value;
+};
+constfrozenbody = document.getElementById("frozenbody");
+frozenbody.oninput = () => {
+  body.style.fill = frozenbody.value;
+};
+constfrozendress = document.getElementById("frozendress");
+frozendress.oninput = () => {
+  dress.style.fill = frozendress.value;
+};
+constfrozenface = document.getElementById("frozenface");
+frozenface.oninput = () => {
+  face.style.fill = frozenface.value;
+};
+constfrozenhair = document.getElementById("frozenhair");
+frozenhair.oninput = () => {
+  hair.style.fill = frozenhair.value;
+};
